a54d4a05bb3936e3da721cc71bd4041b3be68ce3
Pavel <pavel.kovalevski111@gmail.com>
2023-06-17 20:02:58 +0300

    Different factors for X Y axis and function scale

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,10 +17,10 @@ function setValue(el, value) {
 };
 
 function getValues () {
-    plateThickness = document.querySelector("#plate_t").value
-    loadWidth = document.querySelector("#load_w").value
-    beamHeight = document.querySelector("#beam_h").value
-    beamWidth = document.querySelector("#beam_w").value
+    plateThickness = +document.querySelector("#plate_t").value
+    loadWidth = +document.querySelector("#load_w").value
+    beamHeight = +document.querySelector("#beam_h").value
+    beamWidth = +document.querySelector("#beam_w").value
 
     return plateThickness, loadWidth, beamHeight, beamWidth
 }
@@ -28,10 +28,10 @@ function getValues () {
 function createSection() {
     getValues()
 
-    drawSection(plate, loadWidth / calculateFactor(), plateThickness / calculateFactor())
+    drawSection(plate, scale(loadWidth), scale(plateThickness))
 
     if(isBeam() == true) {
-        drawSection(beam, beamWidth / calculateFactor(), beamHeight / calculateFactor())
+        drawSection(beam, scale(beamWidth), scale(beamHeight))
         setValue(beamWidthDim, beamWidth)
         setValue(beamHeightDim, beamHeight)
         beam.style.display = 'block'
@@ -47,12 +47,32 @@ createSectionButton.addEventListener('click', () => {
     createSection()
 });
 
-function calculateFactor() {
-    getValues()
+function calculateFactorX() {
+    let f = 0
 
-    f = loadWidth / 300
+    loadWidth > beamWidth ? f = loadWidth / 300 : f = beamWidth / 300
+
+    console.log(loadWidth, beamWidth, f)
 
     return f
+}
+
+function calculateFactorY() {
+    f = (plateThickness + beamHeight) / 100
+
+    return f
+}
+
+function scale(value) {
+    let factorX = calculateFactorX()
+    let factorY = calculateFactorY()
+    let result = 0
+
+    factorX > factorY ? result = value / factorX : result = value / factorY
+
+    console.log(factorX, factorY)
+
+    return result
 }
 
 function drawSection(el, width, height) {
@@ -86,12 +106,10 @@ calculateLoadButton.addEventListener('click', () => {
 //beam check
 
 function isBeam() {
-    getValues()
-
     /* beamHeight && beamWidth ? a = true : a = false
     return a */
 
-    if(beamHeight && beamHeight) {
+    if(beamHeight && beamWidth) {
         return true
     } else {
         return false
